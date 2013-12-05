87e0c6d2b37a848effc9f22fdd5a9466339ea15b
lupinski <jesselupinski@gmail.com>
2013-12-05 21:11:41 +0000

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Clip::Application.config.secret_key_base = '0bd6f6b5ccd6547fa7081d2fc2ab25c20b26919117135331d229c0c397dd2e4314bc652ec6a0e45a98c262c66ee67463f8579261361487df7682de5aeebe0ca8'
